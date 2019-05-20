4b2868342f68f4949b54fb7efcc95b8b99be52c3
chungfung <fy_java@163.com>
2019-05-20 14:56:02 +0800

    后台管理系统模板

---
+++ system-web/src/main/java/com/system/web/controller/system/SysIndexController.java
@@ -0,0 +1,48 @@
+package com.system.web.controller.system;
+
+import com.system.common.config.Global;
+import com.system.common.core.controller.BaseController;
+import com.system.framework.util.ShiroUtils;
+import com.system.service.domain.SysMenu;
+import com.system.service.domain.SysUser;
+import com.system.service.service.ISysMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+/**
+ * 首页 业务处理
+ * 
+ * @author system
+ */
+@Controller
+public class SysIndexController extends BaseController
+{
+    @Autowired
+    private ISysMenuService menuService;
+
+    // 系统首页
+    @GetMapping("/index")
+    public String index(ModelMap mmap)
+    {
+        // 取身份信息
+        SysUser user = ShiroUtils.getSysUser();
+        // 根据用户id取出菜单
+        List<SysMenu> menus = menuService.selectMenusByUser(user);
+        mmap.put("menus", menus);
+        mmap.put("user", user);
+        mmap.put("copyrightYear", Global.getCopyrightYear());
+        return "index";
+    }
+
+    // 系统介绍
+    @GetMapping("/system/main")
+    public String main(ModelMap mmap)
+    {
+        mmap.put("version", Global.getVersion());
+        return "main";
+    }
+}
